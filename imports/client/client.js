2fb71fc88fe1c96cd6cf659ded9e8d72aa8885cf
Remalyn <nylamer@gmail.com>
2017-03-15 10:35:38 +0800

    App structure and crisp.im

---
+++ imports/client/client.js
@@ -0,0 +1,26 @@
+//import { Template } from 'meteor/templating';
+//import { ReactiveVar } from 'meteor/reactive-var';
+
+//import './main.html'; - removed this because caused error in console
+  document.write("<script type=\"text\/javascript\"> window.$crisp=[];window.CRISP_WEBSITE_ID=\"8e478fe4-030b-421b-8269-c62f3bf5a5f0\";(function(){ d=document;s=d.createElement(\"script\"); s.src=\"https:\/\/client.crisp.im\/l.js\"; s.async=1;d.getElementsByTagName(\"head\")[0].appendChild(s);})(); <\/script>");
+  console.log("This is a test if i am working.");
+//Template.hello.onCreated(function helloOnCreated() {
+  // counter starts at 0
+//  this.counter = new ReactiveVar(0);
+//});
+
+//Template.hello.helpers({
+//  counter() {
+//    return Template.instance().counter.get();
+//  },
+//});
+
+//Template.hello.events({
+//  'click button'(event, instance) {
+    // increment the counter when button is clicked
+//    instance.counter.set(instance.counter.get() + 1);
+//  },
+//});
+
+
+  //crisp.im
